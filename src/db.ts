e1baa089bd12e176a1bbd46eab5da6ebfe7083a0
MajorasCask <vitinsilvad@gmail.com>
2025-09-15 19:32:26 -0300

    #3 - db integration pt.2

--- src/db.ts
+++ src/db.ts
@@ -3,8 +3,8 @@ import { Pool } from "pg";
 const pool = new Pool({
   user: "postgres",       // tu usuario
   host: "localhost",
-  database: "mibase",     // tu base de datos
-  password: "tu_password",
+  database: "puntalink",     // tu base de datos
+  password: "root",
   port: 5432,
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import 'dotenv/config';
 import app from './app.js';
+import pool from './db.js';
 
 const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => {
   console.log(`[puntalink-pro-ts-struct] Servidor en puerto ${PORT}`);
 });
+
+// Funcion Prueba Conexion DB
+async function getUsuarios() {
+  const result = await pool.query("SELECT * FROM usuario");
+  return result.rows;
+}
+
+getUsuarios().then(usuarios => console.log(usuarios));
